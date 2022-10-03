888b578cd4f7b5492e827ae5f7b1f4f22b86a700
ricardo <rgranvilla@gmail.com>
2022-10-03 20:43:02 -0300

    feat(coffee-delivery): using browser router

--- challenges/reactjs/v2/02-coffee-delivery/src/App.tsx
+++ challenges/reactjs/v2/02-coffee-delivery/src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Router } from './Router';
 import { GlobalStyles } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello Coffee Delivery</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   );
--- challenges/reactjs/v2/02-coffee-delivery/src/layouts/DefaultLayout/styles.ts
+++ challenges/reactjs/v2/02-coffee-delivery/src/layouts/DefaultLayout/styles.ts
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
 
 export const LayoutContainer = styled.div`
-  max-width: 74rem;
-  height: calc(100vh - 10rem);
-  margin: 5rem auto;
-  padding: 2.5rem;
-
-  border-radius: 8px;
+  max-width: 70rem;
+  height: 100vh;
+  margin: 0 auto;
 
   display: flex;
   flex-direction: column;
