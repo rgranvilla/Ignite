da86a8e8d1bfdd3d86b049cb37bd0d974a54bf85
ricardo <rgranvilla@gmail.com>
2022-10-04 12:33:30 -0300

    feat(coffee-delivery): product list component created

---
+++ challenges/reactjs/v2/02-coffee-delivery/src/components/ProductList/ProductList.tsx
@@ -0,0 +1,11 @@
+import { ProductListContainer } from './styles';
+
+function ProductList() {
+  return (
+    <ProductListContainer>
+      <h1>Nossos cafés</h1>
+    </ProductListContainer>
+  );
+}
+
+export { ProductList };
--- challenges/reactjs/v2/02-coffee-delivery/src/pages/Home/Home.tsx
+++ challenges/reactjs/v2/02-coffee-delivery/src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import { Intro } from '../../components/Intro';
+import { ProductList } from '../../components/ProductList';
 import { HomeContainer } from './styles';
 
 function Home() {
   return (
     <HomeContainer>
       <Intro />
+      <ProductList />
     </HomeContainer>
   );
 }
