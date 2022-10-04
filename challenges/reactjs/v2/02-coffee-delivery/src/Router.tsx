62a9a2283324fb3add72d75bf26877b5713ebce3
ricardo <rgranvilla@gmail.com>
2022-10-04 18:19:06 -0300

    feat(coffee-delivery): checkout router and page created

--- challenges/reactjs/v2/02-coffee-delivery/src/Router.tsx
+++ challenges/reactjs/v2/02-coffee-delivery/src/Router.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import { DefaultLayout } from './layouts/DefaultLayout';
+
+import { Checkout } from './pages/Checkout';
 import { Home } from './pages/Home';
+
+import { DefaultLayout } from './layouts/DefaultLayout';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
---
+++ challenges/reactjs/v2/02-coffee-delivery/src/pages/Checkout/Checkout.tsx
@@ -0,0 +1,11 @@
+import { CheckoutContainer } from './styles';
+
+function Checkout() {
+  return (
+    <CheckoutContainer>
+      <h1>Checkout</h1>
+    </CheckoutContainer>
+  );
+}
+
+export { Checkout };
