cebff397c7771a33717e1167d379f2f36b99f361
ricardo <rgranvilla@gmail.com>
2022-10-10 14:07:39 -0300

    feat(dt-money): modal transaction style completed

--- courses/reactjs/v2/03-ignite-dt-money/src/components/NewTransactionModal/NewTransactionModal.tsx
+++ courses/reactjs/v2/03-ignite-dt-money/src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -1,7 +1,13 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { X } from "phosphor-react";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
-import { CloseButton, Content, Overlay } from "./styles";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
 
 function NewTransactionModal() {
   return (
@@ -18,6 +24,17 @@ function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="text" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
