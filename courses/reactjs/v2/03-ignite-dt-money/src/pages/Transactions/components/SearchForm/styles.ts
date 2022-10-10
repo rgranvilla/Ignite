e876a8fd185e4f7fb14c17989dd42f7325a49427
ricardo <rgranvilla@gmail.com>
2022-10-10 16:11:27 -0300

    feat(dt-money): applye react hook form and validation to component

--- courses/reactjs/v2/03-ignite-dt-money/src/pages/Transactions/components/SearchForm/styles.ts
+++ courses/reactjs/v2/03-ignite-dt-money/src/pages/Transactions/components/SearchForm/styles.ts
@@ -30,7 +30,12 @@ export const SearchFormContainer = styled.form`
     font-weight: bold;
     border-radius: 6px;
 
-    &:hover {
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       background: ${(props) => props.theme["green-500"]};
       border: 1px solid ${(props) => props.theme["green-500"]};
       color: ${(props) => props.theme.white};
