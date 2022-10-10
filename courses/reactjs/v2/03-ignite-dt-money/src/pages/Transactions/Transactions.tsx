6802bb5dc3f62a4de5f8aaa0830734713b2d2080
ricardo <rgranvilla@gmail.com>
2022-10-10 12:06:02 -0300

    feat(dt-money): summary component created

--- courses/reactjs/v2/03-ignite-dt-money/src/pages/Transactions/Transactions.tsx
+++ courses/reactjs/v2/03-ignite-dt-money/src/pages/Transactions/Transactions.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 function Transactions() {
   return (
     <>
       <Header />
+      <Summary />
     </>
   );
 }
