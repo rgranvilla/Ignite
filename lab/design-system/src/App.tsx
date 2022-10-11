1c06673dbf841ca1b2803c371964f19dafa31d46
ricardo <rgranvilla@gmail.com>
2022-10-11 20:29:30 -0300

    feat(lab-ds): setup global style tailwind css, install storybook

--- lab/design-system/src/App.tsx
+++ lab/design-system/src/App.tsx
@@ -1,8 +1,10 @@
+import "./styles/global.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <h1 className="font-bold text-5xl text-violet-500">Hello World</h1>
+    </>
   );
 }
 
