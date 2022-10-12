89c595d65b853299e6aa032b0f9d8ee64a8609d3
ricardo <rgranvilla@gmail.com>
2022-10-11 22:55:19 -0300

    feat(lab-ds): button component created

---
+++ lab/design-system/src/components/Button.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { ButtonProps, Button } from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  args: {
+    children: "Create account",
+  },
+} as Meta<ButtonProps>;
+
+export const Default: StoryObj<ButtonProps> = {};
