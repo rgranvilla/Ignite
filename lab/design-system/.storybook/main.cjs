9d2983635ab9f965f99fe9e39dd424804d443ade
ricardo <rgranvilla@gmail.com>
2022-10-12 21:13:10 -0300

    ci(lab-ds): add publish storybook workflow

--- lab/design-system/.storybook/main.cjs
+++ lab/design-system/.storybook/main.cjs
@@ -14,7 +14,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/Ignite/tree/master/lab/design-system/";
+      config.base = "/Ignite/";
     }
 
     return config;
